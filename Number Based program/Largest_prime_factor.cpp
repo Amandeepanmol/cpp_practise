1e727aaff9490f3dc3a641d8d4548b25923d02ec
Amandeep Singh <47387270+Amandeepanmol@users.noreply.github.com>
2021-02-03 21:02:30 +0530

    Create Largest_prime_factor.cpp

---
+++ Number Based program/Largest_prime_factor.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h> 
+using namespace std;
+
+ // } Driver Code Ends
+
+
+class Solution{
+public: 
+    long long int largestPrimeFactor(int N){
+        int i=2;
+        while(i<N)
+        {
+            if(N%i==0)
+            {
+                N/=i;
+            }
+            else
+            {
+                i++;
+            }
+        }
+        
+        return i;
+    }
+};
+
+// { Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N;
+        cin>>N;
+        Solution ob;
+        cout << ob.largestPrimeFactor(N) << endl;
+    }
+    return 0; 
+}  // } Driver Code Ends
