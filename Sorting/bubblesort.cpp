36c6e301c6be707c479a6d29e81d903540e3d086
Amandeep Singh <47387270+Amandeepanmol@users.noreply.github.com>
2021-08-10 23:45:53 +0530

    Update bubblesort.cpp

--- Sorting/bubblesort.cpp
+++ Sorting/bubblesort.cpp
@@ -35,3 +35,60 @@ int main()
         cout<<arr[i]<<" ";
     }
 }
+
+/*
+T(c)=O(n^2)
+(n-2)+(n-1)+........2+1
+=>n*(n-1)/2;
+
+
+
+
+===============================
+
+Optimised bubble sort => when array is sorted 
+----------------------------------------------
+#include<bits/stdc++.h>
+using namespace std;
+#define MAX_SIZE 100
+
+int main()
+{
+    int n,arr[MAX_SIZE],i,j,temp=0;
+    cin>>n;
+
+    if(n<0 || n>MAX_SIZE)
+    {
+        cout<<"Invalid range"<<endl;         // if invalid input is enter
+    }
+
+    for(i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+
+    bool flag=false;        // isme jab array sorted hoga toh last tak na dekhga
+    for(i=0;i<n-1;i++)
+    {
+        for(j=0;j<n-i-1;j++)  // further optimise as last elemnt is sorted
+        {
+            if(arr[j]>arr[j+1])            //descending order for ascending just change < to > then
+            {
+                temp=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
+                flag=true;
+            }
+        }
+        
+        if(flag==false)      // jab array  sorted hai toh return out iss case mai iski time complexity O(n) hogyi
+        {
+            break;
+        }
+    }
+    cout<<"Array after sorting is:"<<endl;
+    for(i=0;i<n;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+}
